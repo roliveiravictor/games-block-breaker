53691a182d4eeffb2d3c69802a97689506cc845a
Victor Oliveira <victordext@gmail.com>
2016-01-28 23:08:54 -0300

    PlayMode Header

--- PlayMode.cs
+++ PlayMode.cs
@@ -1,4 +1,18 @@
-﻿using UnityEngine;
+﻿/**********************************************************************/
+/*                                                                    */
+/*                         Block Breaker   							  */
+/*																	  */
+/*		This is the code from a game created to practice my           */
+/*      Unity and Game Development skills. Most of this was           */
+/*      made watching Ben Tristem's tutorial at Udemy.                */
+/*                                                                    */
+/*      For more information please visit:                            */
+/*      https://www.udemy.com/unitycourse/                            */
+/*                                                                    */
+/*                                                                    */
+/**********************************************************************/
+
+using UnityEngine;
 using System.Collections;
 
 public class PlayMode : MonoBehaviour 
